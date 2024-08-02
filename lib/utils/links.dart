e5ceb94d574c216d89d808f68a6f1d2b18769dec
Piyush <piyushverma.809@gmail.com>
2024-08-03 00:44:20 +0530

    email functionality change

--- lib/utils/links.dart
+++ lib/utils/links.dart
@@ -8,7 +8,8 @@ class Links {
   static Uri githubUrl = Uri.parse("https://github.com/android-newbie/");
   static Uri linkedinUrl =
       Uri.parse("https://www.linkedin.com/in/piyush-verma-236253176/");
-  static const emailAddress = "piyushverma.809@gmail.com";
+
+  static Uri emailAddress = Uri.parse("mailto:piyushverma.809@gmail.com");
 
   static void launchResume() async {
     if (await canLaunchUrl(resumeLinK)) {
@@ -19,23 +20,10 @@ class Links {
   }
 
   static void launchEmail() async {
-    final Uri gmailUri = Uri(
-        scheme: 'https',
-        host: 'mail.google.com',
-        path: '/mail/u/0/',
-        queryParameters: {
-          //'view': 'cm',
-          //'fs': '1',
-          'tf': '1',
-          'to': emailAddress,
-          'su': 'Your Subject Here',
-          'body': 'Hello,'
-        });
-
-    if (await canLaunch(gmailUri.toString())) {
-      await launch(gmailUri.toString());
+    if (await canLaunchUrl(emailAddress)) {
+      await launchUrl(emailAddress);
     } else {
-      throw 'Could not launch $gmailUri';
+      throw "could not launch $emailAddress";
     }
   }
 }
